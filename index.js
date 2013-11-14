1e3469cc5b43097f16dedc73db48e326449df9ea
Shallker Wang <shallker.wang@profero.com>
2013-11-14 17:53:12 +0800

    Fix scrollTop on Firefox and Safari

--- index.js
+++ index.js
@@ -19,13 +19,12 @@ module.exports = top;
  *
  * @api public
  */
-
 function top() {
   var el = domify(html);
   var height = window.innerHeight;
 
   function onscroll() {
-    var top = document.body.scrollTop;
+    var top = getScrollTop();
     if (top < height / 2) return hide();
     show();
   }
@@ -40,4 +39,18 @@ function top() {
 
   event.bind(window, 'scroll', debounce(onscroll, 50));
   document.body.appendChild(el);
+}
+
+function getScrollTop(){
+  if (typeof pageYOffset!= 'undefined'){
+    //most browsers except IE before #9
+    return pageYOffset;
+  } else {
+    var B = document.body; //IE 'quirks'
+    var D = document.documentElement; //IE with doctype
+
+    D = (D.clientHeight)? D: B;
+
+    return D.scrollTop;
+  }
 }
